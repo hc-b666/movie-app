e995939fdf36910b6a88c3a130f42160c063a93e
Bobby <bobur0218programmer@gmail.com>
2024-12-05 13:46:57 +0500

    deleted older one, update backend base url

--- MovieAppClient/src/app/environments/environment.ts
+++ MovieAppClient/src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5218/api/v1'
+  apiUrl: 'http://localhost:5029/api/v1'
 };
--- MovieAppServer/Repositories/IUserRepository.cs
+++
@@ -1,8 +0,0 @@
-public interface IUserRepository
-{
-    Task<UserModel> GetUserByEmailAsync(string email);
-
-    Task CreateUserAsync(UserModel user);
-
-    Task<UserDto> GetUserByIdAsync(int id);
-}
